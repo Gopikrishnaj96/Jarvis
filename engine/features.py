0de42ea821caa3e63530ea09e67c3aa42734ec15
Gopikrishnaj96 <gopikrishnaj96@gmail.com>
2025-01-07 09:31:22 +0530

    day3

---
+++ engine/features.py
@@ -0,0 +1,8 @@
+import playsound as playsound
+import eel
+
+#playing assistant  sound function
+"""@eel.expose
+def playAssistantsound():
+    music_dir="www\\assets\\audio\\start_sound.mp3"
+     playsound(music_dir) """
--- www/main.js
+++ www/main.js
@@ -10,4 +10,34 @@ $(document).ready(function () {
 
         },
     });
+    //Siri config
+    
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 810,
+        height: 250,
+        style:"ios9",
+        speed:0.3,
+        amplitude:"1",
+        autostart:true
+    });
+    //siri msg animation
+    $('.siri-message').textillate({
+        loop: true,
+        sync:true,
+        in:{
+            effect: "fadeInUp",
+            sync:true
+        },
+        out:{
+            effect:"fadeOutUp",
+            sync:true,
+        },
+    });
+    $("#MicBtn" ).click(function () { 
+        //eel.playAssistandSound()
+      $("#Oval").attr("hidden", true);
+      $("#SiriWave").attr("hidden", false)
+        
+    });
 });
